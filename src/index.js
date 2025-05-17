2f5de96b783ba7b1af661af51521eca01b14c499
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-05-17 11:45:32 +0400

    basic connection of sockets real time

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import http from "http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+
+io.on("connection", (socket) => {
+  console.log("User connected to (Server)");
+  // console.log(socket);
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () =>
+  console.log(`Server is running on port http://localhost:${PORT}`)
+);
